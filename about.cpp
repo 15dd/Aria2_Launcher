38ea88c068511146613175615a50f5ed55d62c1f
15dd <1572698652@qq.com>
2023-02-12 12:59:49 +0800

    ver1.0 第三版 build 1

--- about.cpp
+++ about.cpp
@@ -7,7 +7,7 @@ about::about(QWidget* parent) :
     ui->setupUi(this);
     this->setFixedSize(this->width(), this->height());
 
-    connect(ui->github, &QPushButton::clicked, [this]() {QDesktopServices::openUrl(QUrl("https://github.com/15dd/Aria2_Launcher", QUrl::TolerantMode)); });
+    connect(ui->github, &QPushButton::clicked, [this]() {QDesktopServices::openUrl(QUrl("https://github.com/15dd/Aria2_Launcher", QUrl::TolerantMode)); }); 
 }
 about::~about() {
     delete ui;
